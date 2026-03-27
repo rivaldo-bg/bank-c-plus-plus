c7862c640ba51112c4957a6c219d193a6bd23730
Rivaldo <rivaldobg@outlook.com>
2026-03-27 15:34:52 +0300

    First version

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+class BankAccount{
+private:
+    string accountHolder;
+    string accountNumber;
+    double balance;
+
+public:
+    //Constructor to initialize the account
+    BankAccount(const string&holder, const string&number)
+    :accountHolder(holder), accountNumber(number), balance(0.0){}
+
+    //Method to deposit money into the account
+    void deposit(double amount){
+       if(amount <= 0){
+        throw invalid_argument("Deposit amount must be positive!");
+       }
+       balance+= amount;
+       cout<< "Deposited: " << amount << " New balance: "<< balance <<endl;
+    }
+
+    //Method to withdraw money from the account
+    void withdraw(double amount){
+       if(amount <= 0){
+        throw invalid_argument("Withdrawal amount must be positive!");
+       }
+       if(amount > balance){
+        throw out_of_range("Insufficient funds");
+       }
+       balance-=amount;
+       cout<< "Withdrew: " <<amount <<" New Balance: "<< balance <<endl;
+    }
+
+    //Method to check the current balance
+    double getBalance() const{
+    return balance;
+    }
+
+    //Method to display account information
+    void displayAccountInfo() const{
+        cout << "Account Holder: " << accountHolder <<endl;
+        cout << "Account Number: " << accountNumber <<endl;
+        cout << "Balance: " <<balance <<endl;
+    }
+
+};
+int main()
+{
+    try{
+        //Create a new bank account for Alice
+        BankAccount myAccount("Alice Johnson", "123456789");
+        myAccount.displayAccountInfo();
+
+        //Performs some transactions
+        myAccount.deposit(500);
+        myAccount.withdraw(200);
+        cout << "Current balance: "<< myAccount.getBalance() <<endl;
+
+        //Attempt to withdraw more than the balance
+        myAccount.withdraw(400);//This will throw an exception
+       }
+    catch(const exception&e){
+       cerr <<"Error: " <<e.what() <<endl;
+       }
+       return 0;
+}
